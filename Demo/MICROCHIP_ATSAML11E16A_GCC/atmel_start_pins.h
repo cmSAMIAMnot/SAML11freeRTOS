f8fc4d428b67f751e2334a8a571fcc62f1f49ab9
Shawn Mosley <smmosle@tycho.nsa.gov>
2019-01-07 13:19:14 -0500

    initial commit on AtmelSAML11 port

---
+++ Demo/MICROCHIP_ATSAML11E16A_GCC/atmel_start_pins.h
@@ -0,0 +1,33 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+#ifndef ATMEL_START_PINS_H_INCLUDED
+#define ATMEL_START_PINS_H_INCLUDED
+
+#include <hal_gpio.h>
+
+// SAML11 has 9 pin functions
+
+#define GPIO_PIN_FUNCTION_A 0
+#define GPIO_PIN_FUNCTION_B 1
+#define GPIO_PIN_FUNCTION_C 2
+#define GPIO_PIN_FUNCTION_D 3
+#define GPIO_PIN_FUNCTION_E 4
+#define GPIO_PIN_FUNCTION_F 5
+#define GPIO_PIN_FUNCTION_G 6
+#define GPIO_PIN_FUNCTION_H 7
+#define GPIO_PIN_FUNCTION_I 8
+
+#define PA00 GPIO(GPIO_PORTA, 0)
+#define PA01 GPIO(GPIO_PORTA, 1)
+#define PA04 GPIO(GPIO_PORTA, 4)
+#define PA05 GPIO(GPIO_PORTA, 5)
+#define PA08 GPIO(GPIO_PORTA, 8)
+#define PA09 GPIO(GPIO_PORTA, 9)
+#define PA10 GPIO(GPIO_PORTA, 10)
+
+#endif // ATMEL_START_PINS_H_INCLUDED
